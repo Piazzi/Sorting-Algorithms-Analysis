7df4b6ec555c78d65eca0e727a2e88022c741d2e
Lucas Piazzi <40416044+Piazzi@users.noreply.github.com>
2021-07-21 12:20:08 -0300

    Cria TAD para rodar os algoritmos

--- main.py
+++ main.py
@@ -30,98 +30,104 @@ from algorithms.heapSort import heapSort
 #comparação, relacione somente os algoritmos não eficientes e eficientes entre si. Por exemplo, não
 #relacione, analise ou compare, o algoritmo BubbleSort com o QuickSort.
 
-# Função que retorna um vetor de entradas de tamanho n, podendo ter números unicos ou não,
-# de acordo com  o que for indicado na variável unique. Sendo inteiro ou float,indicado pela 
-# variável type. E sendo um vetor ordenado, ordenado inversamente ou aleatório de acordo
-# com o que for indicado pela variável order.
-
-# Generates a random n size vector.
-# unique = indicates if the numbers in the array should be unique or not
-# type = indicates whether numbers should be float or integer
-# order = indicates whether the returned array should be ordered, inverse ordered or random.
-def generateEntries(n, unique, type, order):
-    entries = []
-    if unique == True:
-        entries = random.sample(range(-10000, 10000), n)
-    else:
-        i = 0
-        while i < n:
-            entries.append(np.random.randint(-10000, 10000))
-            i = i + 1
+#This is the main ADT of the project, it creates the entries and runs each algorithm based
+# on the parameters receveid.
+class Test(object):
+    """Constructor"""
+    def __init__(self, n, unique, type, order, algorithm):
+        self.n = n
+        self.unique = unique
+        self.type = type
+        self.order = order
+        self.algorithm = algorithm
+        
+        
+    # Generates a random n size vector. The numbers in the vector will be in range between -10000 and 10000.
+    # unique = indicates if the numbers in the array should be unique or not
+    # type = indicates whether numbers should be float or integer
+    # order = indicates whether the returned array should be ordered, inverse ordered or random.
+    def generateEntries(self):
+        entries = []
+        if self.unique == True:
+            entries = random.sample(range(-10000, 10000), self.n)
+        else:
+            i = 0
+            while i < self.n:
+                entries.append(np.random.randint(-10000, 10000))
+                i = i + 1
+                
+        if type == 'float':
+            entries = [x/10 for x in entries]
             
-    if type == 'float':
-        entries = [x/10 for x in entries]
-        
-    if order == 'ascending':
-        entries.sort()
-    elif order == 'descending':
-        entries.sort(reverse=True)
-        
-    print(entries)
-    print('\n')
-    return entries
+        if self.order == 'ascending':
+            entries.sort()
+        elif self.order == 'descending':
+            entries.sort(reverse=True)
+            
+        print(entries)
+        print('\n')
+        return entries
     
-
-#PRINT
-#alist = generateEntries(100, 'false', 'integer', 'random')
-#startTimeBubbleSort = time.time()
-#bubbleSort(alist)
-#endTimeBubbleSort = time.time()
-#print("BUBBLESORT",alist, "bubblesort time: ",endTimeBubbleSort-startTimeBubbleSort , " s" )
-
-#alist = generateEntries(100, 'false', 'integer', 'random')
-#startTimeInsertionSort = time.time()
-#insertionSort(alist)
-#endTimeInsertionSort = time.time()
-#print("INSERTION SORT",alist, "insertion sort time: ",endTimeInsertionSort-startTimeInsertionSort , " s" )
-
-#alist = generateEntries(100, 'false', 'integer', 'random')
-#startTimeSelectionSort = time.time()
-#selectionSort(alist)
-#endTimeSelectionSort = time.time()
-#print("SELECTION SORT",alist, "selection sort time: ",endTimeSelectionSort - startTimeSelectionSort, " s" )
-
-alist = generateEntries(10, False, 'integer', 'random')
-startTimeBogoSort = time.time()
-count = bogoSort(alist)
-endTimeBogoSort = time.time()
-print("BOGO SORT",alist, "bogo sort time: ",endTimeBogoSort - startTimeBogoSort, " s" , "COMPARISONS: ", count)
-
-#alist = generateEntries(100, 'false', 'integer', 'random')
-#startTimeQuickSort = time.time()
-#quickSort(alist)
-#endTimeQuickSort = time.time()
-#qsTime= endTimeQuickSort-startTimeQuickSort
-#print("QUICK SORT",alist, "quicksort time: ",qsTime , " s" )
+    # Run the algorithm for the given parameters
+    def runTest(self):
+        entries = self.generateEntries()
+        startTime = time.time()
+        #each algorithm returns the numbers of comparisons made
+        count = self.algorithm(entries)
+        endTime = time.time()
+        print("Numbers to orderes: ", entries, 'Execution Time: ', endTime - startTime, "s", "Number of Comparisons: ", count)
+        
+        
 
 
-#alist = generateEntries(100, 0, 'integer')
-#startTimeMergeSort = time.time()
-#mergeSort(alist)
-#endTimeMergeSort = time.time()
-#msTime = endTimeMergeSort - startTimeMergeSort
-#print("MERGESORT",alist , "mergesort time: ", msTime, " s" )
-
-
-#alist = generateEntries(100, 0, 'integer')
-#startTimeHeapSort = time.time()
-#heapSort(alist)
-#endTimeHeapSort = time.time()
-#hsTime = endTimeHeapSort - startTimeHeapSort
-#print("HEAPSORT",alist , "heapsort time: ", hsTime, " s" )
-#PRINT 
 
 
 # Data for plotting
-#t = np.arange(0.0, 2.0, 0.01)
-#s = 1 + np.sin(2 * np.pi * t)
+#A ideia é que cada ponto da curva seja fotmado por um valor de n (tamanho do vetor)
+# e o respectivo tempo de processamento (ou numero de comparações).
+
+#Como vocês precisarão analisar diferentes eixos, para cada eixo, vocês devem ter diferentes conjuntos de entrada, 
+# seja pelo tamanho, tipo ou configuração (ordenado, não ordenado ou quase ordenado), 
+# sendo que precisarão de datasets com tamanhos diferentes dentro de cada eixo.
+
+# ------------------------------ BUBBLE SORT ------------------------------ 
+
+test = Test(10, False, 'integer', 'random', bubbleSort)
+test.runTest()
+
+#t = np.arange(endTimeBubbleSort-startTimeBubbleSort, 2000.0, 300000.0)
+#s = np.arange(count, 20000,123123 )
 
 #fig, ax = plt.subplots()
 #ax.plot(t, s)
 
-#ax.set(xlabel='time (s)', ylabel='Iteration',
-#       title='Time x Entries graph')
+#ax.set(xlabel='Tempo (s)', ylabel='N° Comparações',
+#       title='Bubble Sort')
 #ax.grid()
 
-#fig.savefig("test.png")
+#fig.savefig("graphs/bubblesort/test.png")
 #plt.show()
+
+#https://matplotlib.org/stable/gallery/lines_bars_and_markers/categorical_variables.html#sphx-glr-gallery-lines-bars-and-markers-categorical-variables-py
+
+
+# data from United Nations World Population Prospects (Revision 2019)
+# https://population.un.org/wpp/, license: CC BY 3.0 IGO
+# year = [1950, 1960, 1970, 1980, 1990, 2000, 2010, 2018]
+# population_by_continent = {
+#     'africa': [228, 284, 365, 477, 631, 814, 1044, 1275],
+#     'americas': [340, 425, 519, 619, 727, 840, 943, 1006],
+#     'asia': [1394, 1686, 2120, 2625, 3202, 3714, 4169, 4560],
+#     'europe': [220, 253, 276, 295, 310, 303, 294, 293],
+#     'oceania': [12, 15, 19, 22, 26, 31, 36, 39],
+# }
+
+# fig, ax = plt.subplots()
+# ax.stackplot(year, population_by_continent.values(),
+#              labels=population_by_continent.keys())
+# ax.legend(loc='upper left')
+# ax.set_title('World population')
+# ax.set_xlabel('Year')
+# ax.set_ylabel('Number of people (millions)')
+
+# plt.show()
