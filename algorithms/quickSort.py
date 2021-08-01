0b5daab08f69f68dd727eab5369b1bf5cb56bc5e
Camila <ccvieira@ice.ufjf.br>
2021-07-31 22:09:02 -0300

    Update quickSort.py

--- algorithms/quickSort.py
+++ algorithms/quickSort.py
@@ -1,44 +1,78 @@
-#recursivly calls quickSortHelper
-def quickSort(alist):
-   quickSortHelper(alist,0,len(alist)-1)
-#quickSortHelper starts with the same initial case as merge sort
-#(if it's <= 1 it is sorted, else it is partitioned)
-def quickSortHelper(alist,first,last):
-   if first<last:
+# It was implemented quicksort iterative due "Fatal Python error: Cannot recover from stack overflow." 
 
-       splitpoint = partition(alist,first,last)
+count = 0 
+  
+# This function is same in both iterative and recursive
+def partition(arr,l,h):
+    global count 
+    i = ( l - 1 )
+    x = arr[h]
+  
+    for j in range(l , h):
+        count = count + 1
+        if   arr[j] <= x:
+            count = count + 1
+  
+            # increment index of smaller element
+            i = i+1
+            arr[i],arr[j] = arr[j],arr[i]
+  
+    arr[i+1],arr[h] = arr[h],arr[i+1]
+    return (i+1)
+  
+# Function to do Quick sort
+# arr[] --> Array to be sorted,
+# l  --> Starting index,
+# h  --> Ending index
+def quickSort(arr):
+    global count
+    l = 0
+    h = len(arr) - 1
+  
+    # Create an auxiliary stack
+    size = h - l + 1
+    stack = [0] * (size)
+  
+    # initialize top of stack
+    top = -1
+  
+    # push initial values of l and h to stack
+    top = top + 1
+    stack[top] = l
+    top = top + 1
+    stack[top] = h
+  
+    # Keep popping from stack while is not empty
+    while top >= 0:
+        count = count + 1
+  
+        # Pop h and l
+        h = stack[top]
+        top = top - 1
+        l = stack[top]
+        top = top - 1
+  
+        # Set pivot element at its correct position in
+        # sorted array
+        p = partition( arr, l, h )
+  
+        # If there are elements on left side of pivot,
+        # then push left side to stack
+        if p-1 > l:
+            count = count + 1
+            top = top + 1
+            stack[top] = l
+            top = top + 1
+            stack[top] = p - 1
+  
+        # If there are elements on right side of pivot,
+        # then push right side to stack
+        if p+1 < h:
+            count = count + 1
+            top = top + 1
+            stack[top] = p + 1
+            top = top + 1
+            stack[top] = h
 
-       quickSortHelper(alist,first,splitpoint-1)
-       quickSortHelper(alist,splitpoint+1,last)
-
-#
-def partition(alist,first,last):
-   pivotvalue = alist[first]
-
-   leftmark = first+1
-   rightmark = last
-
-   done = False
-   while not done:
-
-       while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
-           leftmark = leftmark + 1
-
-       while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
-           rightmark = rightmark -1
-
-       if rightmark < leftmark:
-           done = True
-       else:
-           temp = alist[leftmark]
-           alist[leftmark] = alist[rightmark]
-           alist[rightmark] = temp
-
-   temp = alist[first]
-   alist[first] = alist[rightmark]
-   alist[rightmark] = temp
-
-
-   return rightmark
-
-
+    return count 
+  
